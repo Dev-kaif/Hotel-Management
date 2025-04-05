65411ecc799b171d81854a776742f4d5189a3699
Mohammad Kaif <kaifghalib123@gmail.com>
2025-04-05 20:27:44 +0530

    added new route to index

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const bookingRoutes = require('./routes/booking');
 const userRoutes = require('./routes/user');
+const reviewRoutes = require('./routes/review');
 
 dotenv.config();
 connectDB(); 
@@ -16,5 +17,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/booking', bookingRoutes);
 app.use('/api/user', userRoutes);
+app.use("/api/reviews", reviewRoutes);
+
 
 app.listen(5000, () => console.log('Server running on port 5000'));
